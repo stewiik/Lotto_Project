94d5f6b2eaf4cafed261968183819c2b7056dc5b
Wiktoria <w.stec19@gmail.com>
2024-10-07 20:32:44 +0200

    added repository, mapper and implementation of facade

--- src/main/java/pl/lotto/domain/resultannouncer/ResultAnnouncerFacade.java
+++ src/main/java/pl/lotto/domain/resultannouncer/ResultAnnouncerFacade.java
@@ -1,4 +1,70 @@
 package pl.lotto.domain.resultannouncer;
 
+import lombok.AllArgsConstructor;
+import pl.lotto.domain.resultannouncer.dto.ResultAnnouncerResponseDto;
+import pl.lotto.domain.resultannouncer.dto.ResultResponseDto;
+import pl.lotto.domain.resultchecker.ResultCheckerFacade;
+import pl.lotto.domain.resultchecker.dto.ResultDto;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Optional;
+
+import static pl.lotto.domain.resultannouncer.ResultMessageResponse.*;
+
+@AllArgsConstructor
 public class ResultAnnouncerFacade {
+
+    private final ResultResponseRepository responseRepository;
+    private final ResultCheckerFacade resultCheckerFacade;
+    private static final LocalTime RESULTS_ANNOUNCEMENT_TIME = LocalTime.of(12, 0).plusMinutes(5);
+    private final Clock clock;
+
+    public ResultAnnouncerResponseDto checkResult(String hash) {
+        if (responseRepository.existsById(hash)) {
+            Optional<ResultResponse> resultResponseCached = responseRepository.findById(hash);
+            if (resultResponseCached.isPresent()) {
+                return new ResultAnnouncerResponseDto(ResultMapper.mapFromResultDtoToResult(resultResponseCached.get()), ALREADY_CHECKED.message);
+            }
+        }
+        ResultDto resultDto = resultCheckerFacade.findByHash(hash);
+        if (resultDto == null) {
+            return new ResultAnnouncerResponseDto(null, HASH_DOES_NOT_EXIST_MESSAGE.message);
+        }
+        ResultResponseDto responseDto = buildResponseDto(resultDto);
+        responseRepository.save(buildResponse(responseDto));
+        if (responseRepository.existsById(hash) && !isAfterResultAnnouncementTime(resultDto)) {
+            return new ResultAnnouncerResponseDto(responseDto, WAIT_MESSAGE.message);
+        }
+        if (resultCheckerFacade.findByHash(hash).isWinner()) {
+            return new ResultAnnouncerResponseDto(responseDto, WIN_MESSAGE.message);
+        }
+        return new ResultAnnouncerResponseDto(responseDto, LOSE_MESSAGE.message);
+    }
+
+    private boolean isAfterResultAnnouncementTime(ResultDto resultDto) {
+        LocalDateTime announcementDateTime = LocalDateTime.of(resultDto.drawDate().toLocalDate(), RESULTS_ANNOUNCEMENT_TIME);
+        return LocalDateTime.now(clock).isAfter(announcementDateTime);
+    }
+
+    private ResultResponse buildResponse(ResultResponseDto resultResponseDto) {
+        return ResultResponse.builder()
+                .hash(resultResponseDto.hash())
+                .numbers(resultResponseDto.numbers())
+                .hitNumbers(resultResponseDto.hitNumbers())
+                .drawDate(resultResponseDto.drawDate())
+                .isWinner(resultResponseDto.isWinner())
+                .build();
+    }
+
+    private ResultResponseDto buildResponseDto(ResultDto resultDto) {
+        return ResultResponseDto.builder()
+                .hash(resultDto.hash())
+                .numbers(resultDto.numbers())
+                .hitNumbers(resultDto.hitNumbers())
+                .drawDate(resultDto.drawDate())
+                .isWinner(resultDto.isWinner())
+                .build();
+    }
 }
---
+++ src/main/java/pl/lotto/domain/resultannouncer/ResultResponse.java
@@ -0,0 +1,14 @@
+package pl.lotto.domain.resultannouncer;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Builder
+record ResultResponse(String hash,
+                      Set<Integer> numbers,
+                      Set<Integer> hitNumbers,
+                      LocalDateTime drawDate,
+                      boolean isWinner) {
+}
