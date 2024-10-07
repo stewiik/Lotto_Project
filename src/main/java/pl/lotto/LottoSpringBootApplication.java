69482c28679e9af7f558a219fa6e33719145f931
Wiktoria <w.stec19@gmail.com>
2024-10-07 21:37:29 +0200

    refactor rest template and added configuration properties

--- src/main/java/pl/lotto/LottoSpringBootApplication.java
+++ src/main/java/pl/lotto/LottoSpringBootApplication.java
@@ -2,8 +2,11 @@ package pl.lotto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pl.lotto.domain.numbergenerator.WinningNumbersGeneratorFacadeConfigProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({WinningNumbersGeneratorFacadeConfigProperties.class})
 public class LottoSpringBootApplication {
 
     public static void main(String[] args) {
--- src/main/java/pl/lotto/domain/numbergenerator/NumbersGeneratorConfig.java
+++ src/main/java/pl/lotto/domain/numbergenerator/NumbersGeneratorConfig.java
@@ -1,10 +1,53 @@
 package pl.lotto.domain.numbergenerator;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import pl.lotto.domain.numberreceiver.NumberReceiverFacade;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Configuration
 public class NumbersGeneratorConfig {
+
+    @Bean
+    public WinningNumbersRepository winningNumbersRepository() {
+        return new WinningNumbersRepository() {
+            @Override
+            public Optional<WinningNumbers> findNumbersByDate(LocalDateTime date) {
+                return Optional.empty();
+            }
+
+            @Override
+            public boolean existsByDate(LocalDateTime nextDrawDate) {
+                return false;
+            }
+
+            @Override
+            public WinningNumbers save(WinningNumbers winningNumbers) {
+                return null;
+            }
+        };
+    }
+
+    @Bean
+    public NumberReceiverFacade numberReceiverFacade() {
+        return new NumberReceiverFacade(null, null, null, null) {
+        };
+    }
+
+    @Bean
+    WinningNumbersGeneratorFacade winningNumbersGeneratorFacade(WinningNumbersRepository winningNumbersRepository, NumberReceiverFacade numberReceiverFacade, RandomNumbersGenerable randomNumberGenerator, WinningNumbersGeneratorFacadeConfigProperties properties) {
+        WinningNumbersValidator winningNumberValidator = new WinningNumbersValidator();
+        return new WinningNumbersGeneratorFacade(randomNumberGenerator, winningNumberValidator, winningNumbersRepository, numberReceiverFacade, properties);
+    }
+
     WinningNumbersGeneratorFacade createForTests(RandomNumbersGenerable generator, WinningNumbersRepository winningNumbersRepository, NumberReceiverFacade numberReceiverFacade) {
-        WinningNumbersValidator validator = new WinningNumbersValidator();
-        return new WinningNumbersGeneratorFacade(numberReceiverFacade, generator, validator, winningNumbersRepository);
+        WinningNumbersGeneratorFacadeConfigProperties properties = WinningNumbersGeneratorFacadeConfigProperties.builder()
+                .upperBand(99)
+                .lowerBand(1)
+                .count(6)
+                .build();
+        return winningNumbersGeneratorFacade(winningNumbersRepository, numberReceiverFacade, generator, properties);
     }
 }
