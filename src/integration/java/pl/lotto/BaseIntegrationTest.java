aeef0dcab18ab142aa35d56008bc35aedf7e7ffe
Wiktoria <w.stec19@gmail.com>
2024-10-10 15:13:49 +0200

    added override bean for adjustable clock

--- src/integration/java/pl/lotto/BaseIntegrationTest.java
+++ src/integration/java/pl/lotto/BaseIntegrationTest.java
@@ -14,16 +14,20 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import pl.lotto.domain.AdjustableClock;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
-@SpringBootTest(classes = LottoSpringBootApplication.class)
+@SpringBootTest(classes = {LottoSpringBootApplication.class, IntegrationConfig.class})
 @ActiveProfiles("integration") // spring uzyje profil o tej nazwie - application-integration
 @AutoConfigureMockMvc //pozwoli zrobic klienta ktory zadaje zapytania do bazy
 @Testcontainers //pozwoli odpali baze na potrzeby testow na dockerze
 public class BaseIntegrationTest {
 
     public static final String WIRE_MOCK_HOST = "http://localhost";
+
+    @Autowired
+    public AdjustableClock clock;
 
     @Autowired
     public MockMvc mockMvc;
